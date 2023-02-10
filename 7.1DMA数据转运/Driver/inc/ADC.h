f10c683d76ecad877a8713d33f7739b4e5316d0a
hpu339 <15513924345@163.com>
2023-02-10 18:11:10 +0800

    7.1DMA

---
+++ 7.1DMA数据转运/Driver/inc/ADC.h
@@ -0,0 +1,9 @@
+#ifndef _ADC_H
+#define _ADC_H
+
+
+void AD_Init();
+uint16_t AD_GetValue(uint8_t ADC_Channel);
+
+#endif // !_ADC_H
+
---
+++ 7.1DMA数据转运/Driver/inc/Key.h
@@ -0,0 +1,8 @@
+
+#ifndef _Key_H
+#define _Key_H
+
+void Key_Init();
+uint8_t Key_IfPress();
+
+#endif // !_Key_H
